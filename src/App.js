be42a822b0e31e33eee93c414007d9580003bf0f
ShrutiDeore76 <shrutideore76@gmail.com>
2024-05-04 21:26:46 +0530

    Adding meals summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Fragment } from 'react';
 
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     <Fragment>
     <Header />
+    <main>
+    <MealsSummary/>
+    </main>
+    
   </Fragment>
   );
 }
